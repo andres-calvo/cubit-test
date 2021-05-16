4b7dfcc8807ccbbcb72807cecb9c30bdced700ef
Andres <andrex200316@gmail.com>
2021-05-16 16:18:30 -0500

    Navegacion e iconos añadidos al Header

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,37 @@
+import { Box, Grid, Link, makeStyles } from "@material-ui/core";
+import { Link as RouterLink } from "react-router-dom";
+
+const useStyles = makeStyles({
+  root: {
+    padding: "0px 1rem",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+});
+
+function Navigation() {
+  return (
+    <Box>
+      <Box>
+        <Link component={RouterLink} to="/">
+          Home
+        </Link>
+        <Link component={RouterLink} to="/question">
+          Question
+        </Link>
+      </Box>
+    </Box>
+  );
+}
+
 function Header() {
-  return <div>Header</div>;
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.root}>
+      <h1>Test</h1>
+      <Navigation></Navigation>
+    </Grid>
+  );
 }
 
 export default Header;
