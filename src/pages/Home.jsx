bd584f9b1e99c87675c43ed9b8035263362ec2db
Andres <andrex200316@gmail.com>
2021-05-16 23:23:36 -0500

    Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,21 @@
+import { Container, makeStyles, useTheme } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    marginTop: "20px",
+    padding: "16px",
+    border: "1px solid #d4d4d4",
+    boxShadow: "-10px 0px 5px 0px rgba(0,0,0,0.26),10px 0px 5px 0px rgba(0,0,0,0.26)",
+  },
+}));
 function Home() {
-  return <div>Home</div>;
+  const themes = useTheme();
+  const classes = useStyles(themes);
+  return (
+    <Container className={classes.wrapper} maxWidth="sm">
+      Hello
+    </Container>
+  );
 }
 
 export default Home;
