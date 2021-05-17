d792e31e03fd32c0c09813737f7821a131582464
Andres <andrex200316@gmail.com>
2021-05-16 22:19:46 -0500

    Responsive main container

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
+import { Container, makeStyles, useTheme } from "@material-ui/core";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./Header";
 import Home from "./pages/Home";
 import Question from "./pages/Question";
 
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "0",
+      paddingRight: "0",
+    },
+  },
+}));
 function App() {
+  const theme = useTheme();
+  const classes = useStyles(theme);
   return (
-    <BrowserRouter>
-      <Header></Header>
-      <Switch>
-        <Route path="/" exact>
-          <Home></Home>
-        </Route>
-        <Route path="/question" exact>
-          <Question></Question>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Container maxWidth="md" className={classes.wrapper}>
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route path="/" exact>
+            <Home></Home>
+          </Route>
+          <Route path="/question" exact>
+            <Question></Question>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </Container>
   );
 }
 
