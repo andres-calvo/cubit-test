97a9873780d5f079a02edaad5c61dfe7069ac0b0
Andres <andrex200316@gmail.com>
2021-05-16 19:48:32 -0500

    Estilos de la barra de navegacion despegable en Mobile

--- src/Header.js
+++ src/Header.js
@@ -3,40 +3,74 @@ import { Link as RouterLink } from "react-router-dom";
 
 import CloseIcon from "@material-ui/icons/Close";
 import MenuIcon from "@material-ui/icons/Menu";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    position: "relative",
     padding: "0px 1rem",
     alignItems: "center",
     justifyContent: "space-between",
+    borderBottom: "1px solid #d4d4d4",
+    boxShadow: "0px 10px 5px 0px rgba(0,0,0,0.26)",
+    [theme.breakpoints.up("sm")]: {
+      justifyContent: "flex-start",
+    },
   },
   nav: {
     display: "none",
+    borderLeft: "1px solid #d4d4d4",
     [theme.breakpoints.up("sm")]: {
       display: "flex",
+      marginLeft: "25%",
       "& a": {
-        margin: "0 8px",
+        margin: "0 16px",
       },
     },
   },
   nav_open: {
-    display: "block",
+    display: "flex",
+    flexFlow: "column",
     position: "absolute",
-    top: "10px",
-    left: "0px",
+    padding: "16px",
+    width: "35%",
+    height: "100vh",
+    background: "white",
+    zIndex: "5",
+    top: "80px",
+    right: "0px",
+    "& a": {
+      margin: "8px 0px",
+    },
+
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
+      position: "relative",
+      top: "0",
+      left: "0",
+    },
+  },
+  nav_close: {
+    display: "none",
   },
 }));
 
 function Navigation(props) {
   const [open, setOpen] = useState(false);
+  console.log(props);
+  function toggleClass(openState) {
+    return openState ? props.classes.nav_open : props.classes.nav_close;
+  }
+  function openMenu() {
+    setOpen(!open);
+  }
   return (
-    <Box>
+    <React.Fragment>
       <Box display={{ xs: "block", sm: "none" }}>
-        {open ? <CloseIcon onClick={() => setOpen(!open)}></CloseIcon> : <MenuIcon onClick={() => setOpen(!open)}></MenuIcon>}
+        {open ? <CloseIcon onClick={() => openMenu()}></CloseIcon> : <MenuIcon onClick={() => openMenu()}></MenuIcon>}
       </Box>
 
-      <Box>
+      <Box className={`${props.classes.nav} ${toggleClass(open)}`}>
         <Link component={RouterLink} to="/">
           Home
         </Link>
@@ -44,7 +78,7 @@ function Navigation(props) {
           Question
         </Link>
       </Box>
-    </Box>
+    </React.Fragment>
   );
 }
 
@@ -54,7 +88,7 @@ function Header() {
   return (
     <Grid container className={classes.root}>
       <h1>Test</h1>
-      <Navigation props={{ theme, classes }}></Navigation>
+      <Navigation width="100%" theme={theme} classes={classes}></Navigation>
     </Grid>
   );
 }
