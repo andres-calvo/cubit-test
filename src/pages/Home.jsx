b0b8a1a69e1921629fc30914088770884949ca1b
Andres <andrex200316@gmail.com>
2021-05-17 19:48:04 -0500

    Estilos al Home para ser responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,24 @@
 import { Container, makeStyles, Paper, useTheme } from "@material-ui/core";
+import CardUser from "../components/CardUser";
 
 const useStyles = makeStyles((theme) => ({
   wrapper: {
     marginTop: "20px",
+  },
+  paper: {
+    padding: "16px",
+    display: "flex",
+    alignItems: "center",
+    flexFlow: "column wrap",
+    justifyContent: "center",
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
+      flexFlow: "row wrap",
+      justifyContent: "flex-start",
+      "& div:nth-child(2n)": {
+        marginLeft: "auto",
+      },
+    },
   },
 }));
 function Home() {
@@ -10,7 +26,23 @@ function Home() {
   const classes = useStyles(themes);
   return (
     <Container className={classes.wrapper} maxWidth="sm">
-      <Paper elevation={4}>Hello</Paper>
+      <Paper elevation={4} className={classes.paper}>
+        <CardUser
+          first_name="andres"
+          last_name="calvo"
+          src={"https://images.pexels.com/photos/1210273/pexels-photo-1210273.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}
+        ></CardUser>
+        <CardUser
+          first_name="andres"
+          last_name="calvo"
+          src={"https://images.pexels.com/photos/1210273/pexels-photo-1210273.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}
+        ></CardUser>
+        <CardUser
+          first_name="andres"
+          last_name="calvo"
+          src={"https://images.pexels.com/photos/1210273/pexels-photo-1210273.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}
+        ></CardUser>
+      </Paper>
     </Container>
   );
 }
