451cecf254fc95f943c46b7d7fdcab419d3e77d5
Andres <andrex200316@gmail.com>
2021-05-17 20:48:53 -0500

    Refactor. Modal inside CardUser

--- src/components/CardUser.jsx
+++ src/components/CardUser.jsx
@@ -1,5 +1,9 @@
-import React from "react";
-import { Button, Card, CardActions, CardContent, CardMedia, makeStyles, Typography, useTheme } from "@material-ui/core";
+import React, { useState } from "react";
+import { Button, Card, CardActions, CardContent, CardMedia, makeStyles, Typography, useTheme, Slide, Dialog, DialogContent } from "@material-ui/core";
+import ContactMailIcon from "@material-ui/icons/ContactMail";
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -15,25 +19,60 @@ const useStyles = makeStyles((theme) => ({
   cardActions: {
     padding: "16px 0",
   },
+  emailWrapper: {
+    display: "flex",
+    alignItems: "center",
+    marginBottom: "16px",
+    "& span": {
+      marginLeft: "16px",
+    },
+  },
 }));
 function CardUser(props) {
+  const [open, setOpen] = useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   const theme = useTheme();
   const classes = useStyles(theme);
-  var text = `${props.first_name} ${props.last_name}`;
+  var name = `${props.first_name} ${props.last_name}`;
+  var email = "andrescalvo@gmail.com";
   return (
-    <Card className={classes.card} elevation={8}>
-      <CardContent>
-        <CardMedia component="img" image={props.src}></CardMedia>
-        <Typography component="p" className={classes.cardText}>
-          {text}
-        </Typography>
-        <CardActions className={classes.cardActions}>
-          <Button size="small" color="primary" variant="contained">
-            Ver mas
-          </Button>
-        </CardActions>
-      </CardContent>
-    </Card>
+    <React.Fragment>
+      <Card className={classes.card} elevation={8}>
+        <CardContent>
+          <CardMedia component="img" image={props.src}></CardMedia>
+          <Typography component="p" className={classes.cardText}>
+            {name}
+          </Typography>
+          <CardActions className={classes.cardActions}>
+            <Button size="small" color="primary" variant="contained" onClick={handleClickOpen}>
+              Ver mas
+            </Button>
+          </CardActions>
+        </CardContent>
+      </Card>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogContent>
+          <img src={props.src} alt="Imagen perfil"></img>
+          <p>{name}</p>
+          <div className={classes.emailWrapper}>
+            <ContactMailIcon color="primary"></ContactMailIcon>
+            <span>{email}</span>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </React.Fragment>
   );
 }
 
