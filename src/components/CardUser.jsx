dbd54bfb9193efb07d8619f27c325dfc0fcc6ecd
Andres <andrex200316@gmail.com>
2021-05-17 19:47:06 -0500

    Cambios en el layot de las CardUser

--- src/components/CardUser.jsx
+++ src/components/CardUser.jsx
@@ -1,16 +1,34 @@
 import React from "react";
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@material-ui/core";
+import { Button, Card, CardActions, CardContent, CardMedia, makeStyles, Typography, useTheme } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+  card: {
+    width: "200px",
+    marginBottom: "16px",
+    [theme.breakpoints.up("sm")]: {
+      marginBottom: "16px",
+    },
+  },
+  cardText: {
+    marginTop: "16px",
+  },
+  cardActions: {
+    padding: "16px 0",
+  },
+}));
 function CardUser(props) {
+  const theme = useTheme();
+  const classes = useStyles(theme);
+  var text = `${props.first_name} ${props.last_name}`;
   return (
-    <Card>
+    <Card className={classes.card} elevation={8}>
       <CardContent>
         <CardMedia component="img" image={props.src}></CardMedia>
-        <Typography component="p">
-          `${props.first_name} ${props.last_name}`
+        <Typography component="p" className={classes.cardText}>
+          {text}
         </Typography>
-        <CardActions>
-          <Button size="small" color="primary">
+        <CardActions className={classes.cardActions}>
+          <Button size="small" color="primary" variant="contained">
             Ver mas
           </Button>
         </CardActions>
